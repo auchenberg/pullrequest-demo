49a6d43dd8238c9ca02ee4d6f9cd2110b02220c8
Peng Lyu <penn.lv@gmail.com>
2018-06-12 11:46:45 -0700

    Let's use const.
    var --> const.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var hbs = require('hbs');
-var session = require('express-session');
+const hbs = require('hbs');
+const session = require('express-session');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
