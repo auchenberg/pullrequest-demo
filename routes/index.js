4c0354f7905d158da2e4718b9ffc1793c13ede8f
Rachel Macfarlane <ramacfar@microsoft.com>
2018-08-10 10:26:12 -0700

    Update route titles

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var products = JSON.parse(fs.readFileSync('./data/products.json', 'utf8'));
 router.get('/', function (_req, res, _next) {
   res.render('index', 
   { 
-    title: 'NodeJS Shopping Cart',
+    title: 'Shopping Cart',
     products: products
   }
   );
@@ -20,7 +20,7 @@ router.get('/add/:id', function(req, res, _next) {
   var productId = req.params.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {});
   var product = products.filter(function(item) {
-    return item.id == productId;
+    return item.id === productId;
   });
   cart.add(product[0], productId);
   req.session.cart = cart;
@@ -36,7 +36,7 @@ router.get('/cart', function(req, res, _next) {
   }
   var cart = new Cart(req.session.cart);
   res.render('cart', {
-    title: 'NodeJS Shopping Cart',
+    title: 'Shopping Cart',
     products: cart.getItems(),
     totalPrice: cart.totalPrice
   });
