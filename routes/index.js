46da705ec9de4dffb5f5e5d13be2a0de56fb9a7a
Rachel Macfarlane <ramacfar@microsoft.com>
2018-09-20 16:54:16 -0700

    Remove some unused variables

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var Cart = require('../models/cart');
 var products = JSON.parse(fs.readFileSync('./data/products.json', 'utf8'));
 
-router.get('/', function (req, res, next) {
+router.get('/', function (_req, res, _next) {
   var productId = products && products[0].id;
 
   res.render('index', 
@@ -17,12 +17,12 @@ router.get('/', function (req, res, next) {
   );
 });
 
-router.get('/add/:id', function(req, res, next) {
+router.get('/add/:id', function(req, res, _next) {
 
   var productId = req.params.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {});
   var product = products.filter(function(item) {
-    return item.id == productId;
+    return item.id === productId;
   });
   cart.add(product[0], productId);
   req.session.cart = cart;
@@ -30,7 +30,7 @@ router.get('/add/:id', function(req, res, next) {
   inline();
 });
 
-router.get('/cart', function(req, res, next) {
+router.get('/cart', function(req, res, _next) {
   if (!req.session.cart) {
     return res.render('cart', {
       products: null
@@ -44,7 +44,7 @@ router.get('/cart', function(req, res, next) {
   });
 });
 
-router.get('/remove/:id', function(req, res, next) {
+router.get('/remove/:id', function(req, res, _next) {
   var productId = req.params.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {});
 
