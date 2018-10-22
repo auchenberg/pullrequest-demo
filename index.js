d1d3aa18c4676f2a718f3b4aef046858ab21f98f
Rachel Macfarlane <ramacfar@microsoft.com>
2018-10-22 11:45:33 -0700

    Make error message more emphatic

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found!');
   err.status = 404;
   next(err);
 });
