6fcb7c23002300f4145c0b21c4b7421d1eef3278
Rachel Macfarlane <ramacfar@microsoft.com>
2018-10-22 11:48:08 -0700

    Undo error message change

--- index.js
+++ index.js
@@ -41,13 +41,13 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found!');
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, _) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
