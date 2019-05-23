3945188e6e8a7d5fd88f75eea3f1a3fdccd7ed4f
github satellite <pjmeyer.at+githubsatellite@outlook.com>
2019-05-23 11:04:39 +0200

    A new commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// new comment2
 // new comment
 var hbs = require('hbs');
 var session = require('express-session');
