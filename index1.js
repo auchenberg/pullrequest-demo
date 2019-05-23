3e806584a3eff5a61cb38ace2f76517ea48e2053
RMacfarlane <ramacfar@microsoft.com>
2019-05-23 16:23:48 -0700

    remove line

--- index1.js
+++ index1.js
@@ -24,7 +24,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-ape.use(console.log());
 app.use(session({
   secret: 'secret',
   resave: false,
