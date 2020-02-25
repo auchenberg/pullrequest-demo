c82ad721bf110f3b70f2cffc4599fdb81814e2cc
Miguel Solorio <miguel.solorio@microsoft.com>
2020-02-25 12:01:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var index = require('./routes/index');
 
 var app = express();
 
+console.log(app)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
