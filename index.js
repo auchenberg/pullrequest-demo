2af0eb79fbbad36f2ca20dc912da87927a9dfc5e
RMacfarlane <ramacfar@microsoft.com>
2021-01-20 17:05:41 -0800

    some changes

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
 }))
 app.use(express.static(path.join(__dirname, 'public')));
 
